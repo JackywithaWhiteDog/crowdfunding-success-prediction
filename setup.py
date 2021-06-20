3ce6bb9fe26f48e9a6d17c4b4e69b252caf48311
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2021-06-20 09:47:19 +0800

    refact: simplify description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.0.1',
-    description='Predicting the success of crowdfuncding projects with CKIP transformer and XGBoost',
+    description='Predicting the success of crowdfuncding projects',
     author='jackywithawhitedog',
     license='GPL-3.0',
 )
